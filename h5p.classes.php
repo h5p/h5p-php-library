ece7b5bcdef0a1a5b0a51764cf8f1986646ab925
Svein-Tore Griff With <with@amendor.com>
2013-09-17 15:57:13 +0200

    Add support for GPL license in libraries

--- h5p.classes.php
+++ h5p.classes.php
@@ -268,7 +268,7 @@ class H5PValidator {
 
   private $libraryOptional  = array(
     'author' => '/^.{1,255}$/',
-    'license' => '/^(cc-by|cc-by-sa|cc-by-nd|cc-by-nc|cc-by-nc-sa|cc-by-nc-nd|pd|cr|MIT)$/',
+    'license' => '/^(cc-by|cc-by-sa|cc-by-nd|cc-by-nc|cc-by-nc-sa|cc-by-nc-nd|pd|cr|MIT|GPL1|GPL2|GPL3)$/',
     'description' => '/^.{1,}$/',
     'dynamicDependencies' => array(
       'machineName' => '/^[\w0-9\-\.]{1,255}$/i',
