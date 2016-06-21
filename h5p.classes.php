8cb0aad09e0c6dc0ade4bcea665074783370988c
andyrandom <andyrandom@3161831.no-reply.drupal.org>
2016-06-21 09:34:27 +0200

    Allow hyphens in HTML tags HFJ-1951

--- h5p.classes.php
+++ h5p.classes.php
@@ -3414,7 +3414,7 @@ class H5PContentValidator {
       return '&lt;';
     }
 
-    if (!preg_match('%^<\s*(/\s*)?([a-zA-Z0-9]+)([^>]*)>?|(<!--.*?-->)$%', $string, $matches)) {
+    if (!preg_match('%^<\s*(/\s*)?([a-zA-Z0-9\-]+)([^>]*)>?|(<!--.*?-->)$%', $string, $matches)) {
       // Seriously malformed.
       return '';
     }
