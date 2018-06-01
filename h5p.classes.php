3ce0adf418a57d68ca00e5c40ea6ddebe888b217
thomasmars <thomasmars87@gmail.com>
2018-06-01 11:09:45 +0200

    HFP-1942 Add crossorigin api call.
    Update minor version of core since interface changes

--- h5p.classes.php
+++ h5p.classes.php
@@ -1773,7 +1773,7 @@ class H5PCore {
   
   public static $coreApi = array(
     'majorVersion' => 1,
-    'minorVersion' => 15
+    'minorVersion' => 16
   );
   public static $styles = array(
     'styles/h5p.css',
