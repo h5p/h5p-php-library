801f3f33c3d9c9b3808dd4eb55fdf00616a1e8e9
Thomas Marstrander <marstranderthomas@gmail.com>
2019-02-21 15:28:04 +0100

    HFP-2654 Add defaultLanguage to exported fields

--- h5p.classes.php
+++ h5p.classes.php
@@ -1671,7 +1671,7 @@ Class H5PExport {
       'embedTypes' => $embedTypes
     );
 
-    foreach(array('authors', 'source', 'license', 'licenseVersion', 'licenseExtras' ,'yearFrom', 'yearTo', 'changes', 'authorComments') as $field) {
+    foreach(array('authors', 'source', 'license', 'licenseVersion', 'licenseExtras' ,'yearFrom', 'yearTo', 'changes', 'authorComments', 'defaultLanguage') as $field) {
       if (isset($content['metadata'][$field]) && $content['metadata'][$field] !== '') {
         if (($field !== 'authors' && $field !== 'changes') || (count($content['metadata'][$field]) > 0)) {
           $h5pJson[$field] = json_decode(json_encode($content['metadata'][$field], TRUE));
