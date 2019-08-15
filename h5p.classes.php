8ceb77e8812af96debe1eeeef3bf4523304ede5f
Paal Joergensen <pal.jorgensen@gmail.com>
2019-08-15 13:26:32 +0200

    Bump core api version

--- h5p.classes.php
+++ h5p.classes.php
@@ -1995,7 +1995,7 @@ class H5PCore {
 
   public static $coreApi = array(
     'majorVersion' => 1,
-    'minorVersion' => 22
+    'minorVersion' => 23
   );
   public static $styles = array(
     'styles/h5p.css',
