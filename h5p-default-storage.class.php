ba63dd7035e56d496521e2423f5c8c1f1b7fd823
yurir <yuri1992@gmail.com>
2021-11-09 21:43:58 +0200

    fix double slash

--- h5p-default-storage.class.php
+++ h5p-default-storage.class.php
@@ -215,7 +215,7 @@ class H5PDefaultStorage implements \H5PFileStorage {
                   if (preg_match("/^(data:|([a-z0-9]+:)?\/)/i", $matches[1]) === 1) {
                     return $matches[0]; // Not relative, skip
                   }
-                  if (strpos(url, "/") == 0) {
+                  if (strpos($cssRelPath, "/") == 0) {
                     // To avoid duplicate double slash
                     $cssRelPath = substr($cssRelPath, 1);
                   }
