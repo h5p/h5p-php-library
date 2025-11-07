e8872480268ce833e813ef96f5fc19435853aa7b
Simen Bergo <bergo.simen@gmail.com>
2025-11-07 11:03:15 +0100

    AU-7490 Use php native methods for escaping

--- embed.php
+++ embed.php
@@ -1,20 +1,20 @@
 <!doctype html>
-<html lang="<?php print esc_attr($lang); ?>" class="h5p-iframe">
+<html lang="<?php echo htmlspecialchars($lang, ENT_QUOTES, 'UTF-8'); ?>" class="h5p-iframe">
 <head>
   <meta charset="utf-8">
-  <title><?php print esc_html($content['title']); ?></title>
+  <title><?php echo htmlspecialchars($content['title'], ENT_QUOTES, 'UTF-8'); ?></title>
   <?php for ($i = 0, $s = count($scripts); $i < $s; $i++): ?>
-    <script src="<?php print esc_url($scripts[$i]); ?>"></script>
+    <script src="<?php echo htmlspecialchars($scripts[$i], ENT_QUOTES, 'UTF-8'); ?>"></script>
   <?php endfor; ?>
   <?php for ($i = 0, $s = count($styles); $i < $s; $i++): ?>
-    <link rel="stylesheet" href="<?php print esc_url($styles[$i]); ?>">
+    <link rel="stylesheet" href="<?php echo htmlspecialchars($styles[$i], ENT_QUOTES, 'UTF-8'); ?>">
   <?php endfor; ?>
-  <?php if (!empty($additional_embed_head_tags)): print implode("\n", $additional_embed_head_tags); endif; ?>
+  <?php if (!empty($additional_embed_head_tags)): echo implode("\n", $additional_embed_head_tags); endif; ?>
 </head>
 <body>
-  <div class="h5p-content" data-content-id="<?php print absint($content['id']); ?>"></div>
+  <div class="h5p-content" data-content-id="<?php echo intval($content['id']); ?>"></div>
   <script>
-    H5PIntegration = <?php print json_encode($integration); ?>;
+    H5PIntegration = <?php echo json_encode($integration); ?>;
   </script>
 </body>
 </html>
