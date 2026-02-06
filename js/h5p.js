7bc813d6a55de35cfb9213cd1bf8fb61a11ec966
Maris <maris.akmentins@h5p.group>
2026-02-06 08:52:44 +0100

    Revert "AU-7270 Ensure iframe inside h5p iframe sends referer on iPhone"
    This reverts commit ab1f4f7dd29150f9dc5ad202c1b8972589d143b3.

--- js/h5p.js
+++ js/h5p.js
@@ -380,7 +380,9 @@ H5P.init = function (target) {
       ? contentData.metadata.defaultLanguage : 'en';
 
     const writeDocument = function () {
-      iframe.srcdoc = '<!doctype html><html class="h5p-iframe" lang="' + contentLanguage + '"><head>' + H5P.getHeadTags(contentId) + '</head><body><div class="h5p-content" data-content-id="' + contentId + '"/></body></html>';
+      iframe.contentDocument.open();
+      iframe.contentDocument.write('<!doctype html><html class="h5p-iframe" lang="' + contentLanguage + '"><head>' + H5P.getHeadTags(contentId) + '</head><body><div class="h5p-content" data-content-id="' + contentId + '"/></body></html>');
+      iframe.contentDocument.close();
     };
 
     $iframe.addClass('h5p-initialized')
